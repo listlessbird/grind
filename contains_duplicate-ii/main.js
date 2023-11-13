19eeb7b93c7f6e0d6df2b67a4d5afaa6ca57fea3
listlessbird <124798751+listlessbird@users.noreply.github.com>
2023-11-13 15:04:42 +0530

    Refactor containsNearbyDuplicate function to use Map instead of Set

--- contains_duplicate-ii/main.js
+++ contains_duplicate-ii/main.js
@@ -4,23 +4,30 @@
  * @return {boolean}
  */
 function containsNearbyDuplicate(nums, k) {
-  const set = new Set()
 
-  
-  for (let i = 0; i < k; i++) {
-    if (set.has(nums[i])) return true
-    set.add(nums[i])
+  const mp = new Map()
+
+  for (let i = 0; i < Math.min(k, nums.length); i++) {
+    if (mp.has(nums[i])) {
+      console.log({ mp, curr: nums[i], i })
+      return true
+    }
+    mp.set(nums[i], i)
   }
 
   for (let i = k; i < nums.length; i++) {
-    if (set.has(nums[i])) return true
-    set.delete(nums[i - k])
-    set.add(nums[i])
+    if (mp.has(nums[i])) {
+      const c = Math.abs(i - mp.get(nums[i]))
+      console.log({ mp, n: nums[i], i, c, k })
+      if (c <= k) return true
+    }
+    mp.delete(nums[i - k])
+    mp.set(nums[i], i)
   }
-  console.log({ set })
   return false
 }
 
-// console.log(containsNearbyDuplicate([1, 2, 3, 1], 3))
-// console.log(containsNearbyDuplicate([1, 2, 3, 1, 2, 3], 2))
+console.log(containsNearbyDuplicate([1, 2, 3, 1], 3))
+console.log(containsNearbyDuplicate([1, 2, 3, 1, 2, 3], 2))
 console.log(containsNearbyDuplicate([1, 2, 1], 0))
+console.log(containsNearbyDuplicate([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 15))
