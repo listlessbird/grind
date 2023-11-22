0172de567eb697909a38841ab50885c006727ff0
listlessbird <124798751+listlessbird@users.noreply.github.com>
2023-11-22 23:28:40 +0530

    Add solution for Max Consecutive Ones III problem

---
+++ max-consecutive-ones-iii/main.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+function longestOnes(nums, k) {
+  let longest = 0
+
+  let left = 0
+  let right = 0
+
+  while (right < nums.length) {
+    if (nums[right] === 0) {
+      k--
+    }
+
+    if (k < 0) {
+      if (nums[left] === 0) k++
+
+      left++
+    }
+    right++
+  }
+
+  return right - left
+}
+
+console.log(longestOnes([1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0], 2))
---
+++ max-consecutive-ones-iii/problem.md
@@ -0,0 +1,1 @@
+[**1004. Max Consecutive Ones III**](https://leetcode.com/problems/max-consecutive-ones-iii/description)
