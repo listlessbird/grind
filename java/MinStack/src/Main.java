dcdd9945ba6218a79fe8650bc15b837b3cebb407
listlessbird <124798751+listlessbird@users.noreply.github.com>
2024-02-10 20:11:04 +0530

    chore: restructure

--- java/MinStack/src/Main.java
+++ java/MinStack/src/Main.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        MinStack obj = new MinStack();
+        obj.push(2);
+        obj.push(0);
+        obj.push(3);
+        obj.push(0);
+        System.out.println(obj.getMin()); // Output: 0
+        obj.pop();
+        System.out.println(obj.getMin()); // Output: 0
+        obj.pop();
+        System.out.println(obj.getMin()); // Output: 0
+        obj.pop();
+        System.out.println(obj.getMin()); // Output: 2
+    }
 }
--- java/MinStack/src/MinStack.java
+++ java/MinStack/src/MinStack.java
@@ -1,2 +1,41 @@
-package PACKAGE_NAME;public class MinStack {
+import java.util.ArrayList;
+
+public class MinStack {
+    private ArrayList<Integer> stack;
+    private ArrayList<Integer> auxStack;
+    public MinStack() {
+        this.stack = new ArrayList<>(10);
+//        to track the min element
+        this.auxStack = new ArrayList<Integer>(10);
+    }
+
+    public void push(int val) {
+        if(!auxStack.isEmpty()) {
+            int lastMin = getMin();
+            if(val <= lastMin) auxStack.add(val);
+        } else {
+            auxStack.add(val);
+        }
+
+        this.stack.add(val);
+    }
+
+    public void pop() {
+
+        if(stack.isEmpty()) {
+            return;
+        }
+        if(top() == getMin()) auxStack.remove(auxStack.size() - 1);
+        stack.remove(stack.size() - 1);
+    }
+
+    public int top() {
+        return this.stack.get(this.stack.size() - 1);
+    }
+    public int getMin() {
+        if(auxStack.isEmpty()) {
+            return  0;
+        }
+        return auxStack.get(auxStack.size() - 1);
+    }
 }
