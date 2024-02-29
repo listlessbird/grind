3f23cb16a60538c1ee0dd7f823cda5710efb463f
listlessbird <124798751+listlessbird@users.noreply.github.com>
2024-02-29 14:25:14 +0530

    feat: add java solution for leetcode 287

---
+++ java/LinkedList/FindDuplicate/src/Solution.java
@@ -0,0 +1,22 @@
+public class Solution {
+    public int findDuplicate(int[] nums) {
+
+        int slow = 0;
+        int fast = 0;
+
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow != fast);
+
+        fast = 0;
+
+        while(slow != fast) {
+            fast = nums[fast];
+            slow = nums[slow];
+        }
+
+        return slow;
+
+    }
+}
